0372fc1045ae32b1709cace39a65dab88cf33e42
IvanTselikov <ivantselikov@yandex.ru>
2023-03-04 00:06:04 +0300

    Выполнено упражнение 3.1

--- index.php
+++ index.php
@@ -7,44 +7,74 @@ if ($db->connect_error) {
     die('Невозможно подключиться к базе данных.');
 }
 
-if (!empty($_POST)) {
-    switch ($_POST['buttonId']) {
+if (!empty($_GET)) {
+    switch ($_GET['buttonId']) {
         case 'ex1':
-            $res = $db->query('SELECT `lastName` FROM `users`') or die ($db->error);
+            $res = $db->query('SELECT `lastName` FROM `users`') or die($db->error);
             while ($row = $res->fetch_assoc()) {
                 echo $row['lastName'], '<br>';
             }
             $res->free();
             break;
         case 'ex2':
-            $res = $db->query('SELECT COUNT(*) FROM `users`') or die ($db->error);
+            $res = $db->query('SELECT COUNT(*) FROM `users`') or die($db->error);
             $count = $res->fetch_array()[0];
             echo '<b>Всего зарегистрировано пользователей:</b> ', $count, '<br>';
             $res->free();
 
-            $date_array = getdate();
-            $begin_date = date('Y-m-d', mktime(0, 0, 0, $date_array['mon'], 1, $date_array['year']));
-            $end_date = date('Y-m-d', mktime(0, 0, 0, $date_array['mon'] + 1, 0, $date_array['year']));
+            $dateArray = getdate();
+            $beginDate = date('Y-m-d', mktime(0, 0, 0, $dateArray['mon'], 1, $dateArray['year']));
+            $endDate = date('Y-m-d', mktime(0, 0, 0, $dateArray['mon'] + 1, 0, $dateArray['year']));
 
             $res = $db->query("SELECT COUNT(*) FROM `users`
                                WHERE `registrationData`
-                                 BETWEEN '$begin_date' AND '$end_date'") or die ($db->error);
+                                 BETWEEN '$beginDate' AND '$endDate'") or die($db->error);
             $count = $res->fetch_array()[0];
             echo '<b>За последний месяц:</b> ', $count, '<br>';
             $res->free();
 
             $res = $db->query('SELECT `firstName`, `lastName`, `login` FROM `users`
-                               ORDER BY `registrationData` DESC LIMIT 0,1') or die ($db->error);
+                               ORDER BY `registrationData` DESC LIMIT 0,1') or die($db->error);
             $row = $res->fetch_assoc();
 
             echo '<b>Последний зарегистрированный пользователь:</b> ', $row['login'],
-                ' (', $row['firstName'], ' ', $row['lastName'], ')';
+                ' (', $row['firstName'], ' ', $row['lastName'], ')', '<br>';
 
             $res->free();
 
             break;
         case 'ex3':
-            echo 'ex3<br>';
+            $userSearch = trim($_GET['userSearch']);
+            if (!empty($userSearch)) {
+                $stmt = $db->prepare("SELECT * FROM `users`
+                    WHERE `firstName` LIKE CONCAT('%',?,'%')
+                        OR `lastName` LIKE CONCAT('%',?,'%')
+                        OR `email` LIKE CONCAT('%',?,'%')
+                        OR `login` LIKE CONCAT('%',?,'%')
+                        OR `description` LIKE CONCAT('%',?,'%')");
+                $stmt->bind_param("sssss", $userSearch, $userSearch, $userSearch, $userSearch, $userSearch);
+                $stmt->execute();
+
+                $res = $stmt->get_result();
+                
+                if ($res->num_rows) {
+                    echo '<ul>';
+                    while ($user = $res->fetch_assoc()) {
+                        echo "<li><b>Логин:</b> {$user['login']}<ul>";
+                        echo "<li><i>Имя:</i> {$user['firstName']}</li>";
+                        echo "<li><i>Фамилия:</i> {$user['lastName']}</li>";
+                        echo "<li><i>Email:</i> {$user['email']}</li>";
+                        echo "<li><i>О себе:</i> {$user['description']}</li>";
+                        echo '</ul></li>';
+                    }
+                    echo '</ul><br>';
+                } else {
+                    echo '<b>Ничего не найдено!</b>', '<br>';
+                }
+                $res->free();
+            }
+            break;
+        default:
             break;
     }
     die();
--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
-  // упр 1
-  $("button").click(function () {
+  // упр 1, 2
+  $("#ex1, #ex2").click(function () {
     $.ajax({
       url: "index.php",
-      type: "POST",
+      type: "GET",
       data: { buttonId: $(this).attr("id") },
-      success: function(response) {
+      success: function (response) {
         $("#output").html(response);
-      }
+      },
+    });
+  });
+
+  // упр 3
+  $("#ex3").click(function() {
+    let search = prompt("Введите строку для поиска:");
+    $.ajax({
+      url: "index.php",
+      type: "GET",
+      data: { buttonId: $(this).attr("id"), userSearch: search },
+      success: function (response) {
+        $("#output").html(response);
+      },
     });
   });
 });
